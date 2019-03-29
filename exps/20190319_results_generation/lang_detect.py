c830abc9c626ee1dcea4ac24d8d996fa650f4fbb
yzjing <jingyz42@gmail.com>
2019-03-29 16:34:39 -0400

    move some scripts to results generation directory

---
+++ exps/20190319_results_generation/lang_detect.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.append('/nfs/nfs7/home/jingy/.local/lib/python3.5/site-packages')
+
+import spacy
+from spacy_cld import LanguageDetector
+import en_core_web_md
+import pandas as pd
+
+nlp = en_core_web_md.load()
+language_detector = LanguageDetector()
+nlp.add_pipe(language_detector)
+
+
+def detect_Eng(text):
+    print(text[0:50])
+    # doc = nlp(line)
+    # if doc._.languages == ['en']:
+    #     cnt_en += 1
+    # cnt_all += 1
+
+
+for fandom in fandoms:
+    df = pd.read_csv('../' + fandom + '_preprocessed.tsv', sep = '\t')
+    df = df.apply(lambda row: detect_Eng(row['Text']), axis=1)
+    break
+
+
+# print(doc._.languages)  # ['en']
+# print(doc._.language_scores['en'])  # 0.96
+
+
+fandoms = [
+'les_miserables_all_media_types',
+'star_wars_all_media_types',
+'harry_potter',
+'sherlock_holmes_&_related_fandoms',
+'marvel',
+'shakespare_william_works',
+'bishoujo_senshi_sailor_moon',
+'haikyuu',
+'hamilton_miranda',
+'kuroko_no_basuke',
+'the_walking_dead_&_related_fandoms',
+'hetalia_axis_powers',
+'naruto',
+'buffy_the_vampire_slayer',
+'arthurian_mythology_&_related_fandoms',
+'ms_paint_adventures',
+'one_direction',
+'attack_on_titan',
+'doctor_who_&_related_fandoms',
+'tolkien_j_r_r_works_&_related_fandoms',
+'dcu',
+'dragon_age_all_media_types',
+'supernatural',
+]
