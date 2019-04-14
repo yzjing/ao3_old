61e33d694ca3295ca2d830b4b6565f901c3556cd
yzjing <jingyz42@gmail.com>
2019-04-14 17:09:00 -0400

    update code

--- exps/20190319_results_generation/gam.r
+++ exps/20190319_results_generation/gam.r
@@ -1,22 +1,62 @@
 library(mgcv)
 df <- read.csv(file='/Users/a307/Desktop/work/current/ao3/exps/20190319_results_generation/fanfic_regression_data_curated.tsv', header=TRUE, sep='\t')
 
-res <- mgcv::bam(Kudos ~ s(Term_novelty, bs='cr', sp=0.0001, k=20) + s(Topic_novelty, bs='cr', sp=0.0001, k=20) + s(Chapters, sp=0.1, bs='cr') + s(author_fic_cnt, sp=0.1, bs='cr') + s(History, sp=0.1, bs='cr') + s(Words, sp=0.1, bs='cr')
-+  + Freq_relationship + Category_F_F + Category_F_M + Category_Gen + Category_M_M
-+  + Category_Multi + Category_Other + ArchiveWarnings_underage + ArchiveWarnings_death + 
+df_kudos <- df[df$Kudos != 0, ]
+ res_kudos <- mgcv::bam(Kudos ~ s(Term_novelty, bs='cr', sp=0.1,k=7) + s(Topic_novelty, bs='cr', sp=0.1,k=5) + s(Chapters, sp=0.1, bs='cr') + s(author_fic_cnt, sp=0.1, bs='cr') 
++  Freq_relationship + Category_F_F + Category_F_M + Category_Gen + Category_M_M
++  Category_Multi + Category_Other + ArchiveWarnings_underage + ArchiveWarnings_death + 
 + ArchiveWarnings_choose_no + ArchiveWarnings_no_apply + ArchiveWarnings_violence + Rating_E                  
-+ + Rating_G + Rating_M + Rating_N + Fandom_harry_potter + Fandom_dcu + Fandom_doctor_who + Fandom_star_wars          
-+ + Fandom_arthurian + Fandom_supernatural + Fandom_haikyuu  + Fandom_kuroko_no_basuke + Fandom_hamilton_miranda 
-+ + Fandom_dragon_age + Fandom_the_walking_dead + Fandom_buffy  + Fandom_les_miserables + Fandom_naruto + Fandom_tolkien 
-+ +  Fandom_shakespare + Fandom_hetalia + Fandom_attack_on_titan + Fandom_ms_paint_adventures + Fandom_marvel             
-+ + Fandom_sailor_moon+ Fandom_one_direction, data=df, method='REML', family='scat')
++ Rating_G + Rating_M + Rating_N + Fandom_harry_potter + Fandom_dcu + Fandom_doctor_who + Fandom_star_wars          
++ Fandom_arthurian + Fandom_supernatural + Fandom_haikyuu  + Fandom_kuroko_no_basuke + Fandom_hamilton_miranda 
++ Fandom_dragon_age + Fandom_the_walking_dead + Fandom_buffy  + Fandom_les_miserables + Fandom_naruto + Fandom_tolkien 
++ Fandom_shakespare + Fandom_hetalia + Fandom_attack_on_titan + Fandom_ms_paint_adventures + Fandom_marvel             
++ Fandom_sailor_moon+ Fandom_one_direction, data=df_kudos, method='REML', family='gaussian')
+
+df_hits <- df[df$Hits != 0, ]
+ res_hits <- mgcv::bam(Hits ~ s(Term_novelty, bs='cr', sp=0.1,k=7) + s(Topic_novelty, bs='cr', sp=0.1,k=5) + s(Chapters, sp=0.1, bs='cr') + s(author_fic_cnt, sp=0.1, bs='cr') 
++ Freq_relationship + Category_F_F + Category_F_M + Category_Gen + Category_M_M
++ Category_Multi + Category_Other + ArchiveWarnings_underage + ArchiveWarnings_death + 
+ArchiveWarnings_choose_no + ArchiveWarnings_no_apply + ArchiveWarnings_violence + Rating_E                  
++ Rating_G + Rating_M + Rating_N + Fandom_harry_potter + Fandom_dcu + Fandom_doctor_who + Fandom_star_wars          
++ Fandom_arthurian + Fandom_supernatural + Fandom_haikyuu  + Fandom_kuroko_no_basuke + Fandom_hamilton_miranda 
++ Fandom_dragon_age + Fandom_the_walking_dead + Fandom_buffy  + Fandom_les_miserables + Fandom_naruto + Fandom_tolkien 
++ Fandom_shakespare + Fandom_hetalia + Fandom_attack_on_titan + Fandom_ms_paint_adventures + Fandom_marvel             
++ Fandom_sailor_moon+ Fandom_one_direction, data=df_hits, method='REML', family='gaussian')
+
+df_comments <- df[df$Comments != 0, ]
+res_comments <- mgcv::bam(Comments ~ s(Term_novelty, bs='cr', sp=0.1,k=7) + s(Topic_novelty, bs='cr', sp=0.1,k=5) + s(Chapters, sp=0.1, bs='cr') + s(author_fic_cnt, sp=0.1, bs='cr') 
++ Freq_relationship + Category_F_F + Category_F_M + Category_Gen + Category_M_M
++ Category_Multi + Category_Other + ArchiveWarnings_underage + ArchiveWarnings_death
++ ArchiveWarnings_choose_no + ArchiveWarnings_no_apply + ArchiveWarnings_violence + Rating_E                  
++ Rating_G + Rating_M + Rating_N + Fandom_harry_potter + Fandom_dcu + Fandom_doctor_who + Fandom_star_wars          
++ Fandom_arthurian + Fandom_supernatural + Fandom_haikyuu  + Fandom_kuroko_no_basuke + Fandom_hamilton_miranda 
++ Fandom_dragon_age + Fandom_the_walking_dead + Fandom_buffy  + Fandom_les_miserables + Fandom_naruto + Fandom_tolkien 
++ Fandom_shakespare + Fandom_hetalia + Fandom_attack_on_titan + Fandom_ms_paint_adventures + Fandom_marvel             
++ Fandom_sailor_moon+ Fandom_one_direction, data=df_comments, method='REML', family='gaussian')
+
+df_bookmarks <- df[df$Bookmarks != 0, ]
+ res_bookmarks <- mgcv::bam(Hits ~ s(Term_novelty, bs='cr', sp=0.1,k=7) + s(Topic_novelty, bs='cr', sp=0.1,k=5) + s(Chapters, sp=0.1, bs='cr') + s(author_fic_cnt, sp=0.1, bs='cr') 
++ Freq_relationship + Category_F_F + Category_F_M + Category_Gen + Category_M_M
++ Category_Multi + Category_Other + ArchiveWarnings_underage + ArchiveWarnings_death
++ ArchiveWarnings_choose_no + ArchiveWarnings_no_apply + ArchiveWarnings_violence + Rating_E                  
++ Rating_G + Rating_M + Rating_N + Fandom_harry_potter + Fandom_dcu + Fandom_doctor_who + Fandom_star_wars          
++ Fandom_arthurian + Fandom_supernatural + Fandom_haikyuu  + Fandom_kuroko_no_basuke + Fandom_hamilton_miranda 
++ Fandom_dragon_age + Fandom_the_walking_dead + Fandom_buffy  + Fandom_les_miserables + Fandom_naruto + Fandom_tolkien 
++ Fandom_shakespare + Fandom_hetalia + Fandom_attack_on_titan + Fandom_ms_paint_adventures + Fandom_marvel             
++ Fandom_sailor_moon+ Fandom_one_direction, data=df_bookmarks, method='REML', family='gaussian')
 
 
-
-
-     
-  
-                
+pdf('/Users/a307/Desktop/rplot_2.pdf', width=16, height=8)
+par(mfrow=c(2,4))
+plot(res_kudos, scale=0, shade=TRUE, select=1, xlab='Term novelty', ylab='Kudos', cex.lab=1.5, cex.axis=1.5)
+plot(res_hits, scale=0, shade=TRUE, select=1, xlab='Term novelty', ylab='Hits', cex.lab=1.5, cex.axis=1.5)
+plot(res_comments, scale=0, shade=TRUE,  select=1, xlab='Term novelty', ylab='Comments', cex.lab=1.5, cex.axis=1.5)
+plot(res_bookmarks, scale=0, shade=TRUE, select=1, xlab='Term novelty', ylab='Bookmarks', cex.lab=1.5, cex.axis=1.5)
+plot(res_kudos, scale=0, shade=TRUE, select=2, xlab='Topic novelty', ylab='Kudos', cex.lab=1.5, cex.axis=1.5)
+plot(res_hits, scale=0, shade=TRUE, select=2, xlab='Topic novelty', ylab='Hits', cex.lab=1.5, cex.axis=1.5)
+plot(res_comments, scale=0, shade=TRUE,  select=2, xlab='Topic novelty', ylab='Comments', cex.lab=1.5, cex.axis=1.5)
+plot(res_bookmarks, scale=0, shade=TRUE, select=2, xlab='Topic novelty', ylab='Bookmarks', cex.lab=1.5, cex.axis=1.5)
+dev.off()
         
 
 
